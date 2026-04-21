a6b5e5bd309f9db494ace07af91bd6ca4ad55547
KeylessServices <patrick27701@hotmail.com>
2026-04-21 09:30:04 -0700

    fixed french not loading

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/',
   plugins: [
     injectHTML(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        fr: resolve(__dirname, 'fr.html'),
+        quote: resolve(__dirname, 'quote.html'),
+        'quote-fr': resolve(__dirname, 'quote-fr.html'),
+      },
+    },
+  },
 });
